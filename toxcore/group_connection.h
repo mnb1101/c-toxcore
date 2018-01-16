8fdc2473fb5e2a70029ea8f3a4d338ce8c04572e
ingvar1995 <igorn1995@yandex.ru>
2018-01-17 00:25:11 +0300

    oob instead of plain connection

--- toxcore/group_connection.h
+++ toxcore/group_connection.h
@@ -67,6 +67,7 @@ struct GC_Connection {
     bool        handshaked; /* true if we've successfully handshaked with this peer */
     uint64_t    pending_handshake;
     uint8_t     pending_handshake_type;
+    bool        is_oob_handshake;
     bool        confirmed;  /* true if this peer has given us their info */
 };
 
