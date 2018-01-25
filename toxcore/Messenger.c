d4757305042911032f27e124b02d2e46cf4bb01c
ingvar1995 <igorn1995@yandex.ru>
2018-01-25 23:11:19 +0300

    gc ann cleanup and refactoring

--- toxcore/Messenger.c
+++ toxcore/Messenger.c
@@ -2708,6 +2708,7 @@ void do_messenger(Messenger *m, void *userdata)
     do_onion_client(m->onion_c);
     do_friend_connections(m->fr_c, userdata);
     do_gc(m->group_handler, userdata);
+    do_gca(m->mono_time, m->group_announce);
     do_friends(m, userdata);
     update_gc_friends_data(m);
     connection_status_callback(m, userdata);
