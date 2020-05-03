2a488b9215a2d88df9f48b95dbdabfc4e396d9c7
iphydf <iphydf@users.noreply.github.com>
2020-05-03 17:49:36 +0100

    Updates from formatters.

--- other/analysis/gen-file.sh
+++ other/analysis/gen-file.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 CPPFLAGS="$CPPFLAGS -DMIN_LOGGER_LEVEL=LOGGER_LEVEL_TRACE"
 CPPFLAGS="$CPPFLAGS -isystem /usr/include/opus"
@@ -16,30 +16,30 @@ LDFLAGS="$LDFLAGS -lopus -lsodium -lvpx -lpthread -lconfig"
 
 put() {
   if [ "$SKIP_LINES" = "" ]; then
-    echo "#line 1 \"$1\"" >> amalgamation.cc
+    echo "#line 1 \"$1\"" >>amalgamation.cc
   fi
-  cat "$1" >> amalgamation.cc
+  cat "$1" >>amalgamation.cc
 }
 
 putmain() {
-  echo "namespace $(echo "$1" | sed -e 's/[^a-zA-Z0-9_]/_/g') {" >> amalgamation.cc
+  echo "namespace $(echo "$1" | sed -e 's/[^a-zA-Z0-9_]/_/g') {" >>amalgamation.cc
   if [ "$SKIP_LINES" = "" ]; then
-    echo "#line 1 \"$1\"" >> amalgamation.cc
+    echo "#line 1 \"$1\"" >>amalgamation.cc
   fi
-  sed -e 's/^int main(/static &/' "$1" >> amalgamation.cc
-  echo "} //  namespace $(echo "$1" | sed -e 's/[^a-zA-Z0-9_]/_/g')" >> amalgamation.cc
+  sed -e 's/^int main(/static &/' "$1" >>amalgamation.cc
+  echo "} //  namespace $(echo "$1" | sed -e 's/[^a-zA-Z0-9_]/_/g')" >>amalgamation.cc
 }
 
 callmain() {
-  echo "  call($(echo "$1" | sed -e 's/[^a-zA-Z0-9_]/_/g')::main, argc, argv);" >> amalgamation.cc
+  echo "  call($(echo "$1" | sed -e 's/[^a-zA-Z0-9_]/_/g')::main, argc, argv);" >>amalgamation.cc
 }
 
-:> amalgamation.cc
+: >amalgamation.cc
 
-echo "#include <algorithm>" >> amalgamation.cc
-echo "#include <cstdio>" >> amalgamation.cc
-echo "#include <memory>" >> amalgamation.cc
-echo "#include <random>" >> amalgamation.cc
+echo "#include <algorithm>" >>amalgamation.cc
+echo "#include <cstdio>" >>amalgamation.cc
+echo "#include <memory>" >>amalgamation.cc
+echo "#include <random>" >>amalgamation.cc
 
 put auto_tests/check_compat.h
 
@@ -54,33 +54,35 @@ FIND_QUERY="$FIND_QUERY -and -not -name av_test.c"
 FIND_QUERY="$FIND_QUERY -and -not -name dht_test.c"
 FIND_QUERY="$FIND_QUERY -and -not -name version_test.c"
 
-(for i in $(eval "$FIND_QUERY"); do
-  grep -o '#include <[^>]*>' "$i" \
-    | grep -E -v '<win|<ws|<iphlp|<libc|<mach/|<crypto_|<randombytes|<u.h>|<sys/filio|<linux'
-done) | sort -u >> amalgamation.cc
+readarray -t FILES <<<"$(eval "$FIND_QUERY")"
 
-echo 'namespace {' >> amalgamation.cc
-for i in $(eval "$FIND_QUERY"); do
+(for i in "${FILES[@]}"; do
+  grep -o '#include <[^>]*>' "$i" |
+    grep -E -v '<win|<ws|<iphlp|<libc|<mach/|<crypto_|<randombytes|<u.h>|<sys/filio|<linux'
+done) | sort -u >>amalgamation.cc
+
+echo 'namespace {' >>amalgamation.cc
+for i in "${FILES[@]}"; do
   if ! grep -q '^int main(' "$i"; then
     put "$i"
   fi
 done
 
-for i in $(eval "$FIND_QUERY"); do
+for i in "${FILES[@]}"; do
   if grep -q '^int main(' "$i"; then
     putmain "$i"
   fi
 done
 
-echo "static void call(int m(), int argc, char **argv) { m(); }" >> amalgamation.cc
-echo "static void call(int m(int, char **), int argc, char **argv) { m(argc, argv); }" >> amalgamation.cc
-echo '}  // namespace' >> amalgamation.cc
+echo "static void call(int m(), int argc, char **argv) { m(); }" >>amalgamation.cc
+echo "static void call(int m(int, char **), int argc, char **argv) { m(argc, argv); }" >>amalgamation.cc
+echo '}  // namespace' >>amalgamation.cc
 
-echo "int main(int argc, char **argv) {" >> amalgamation.cc
-for i in $(eval "$FIND_QUERY"); do
+echo "int main(int argc, char **argv) {" >>amalgamation.cc
+for i in "${FILES[@]}"; do
   if grep -q '^int main(' "$i"; then
     callmain "$i"
   fi
 done
-echo "  return 0;" >> amalgamation.cc
-echo "}" >> amalgamation.cc
+echo "  return 0;" >>amalgamation.cc
+echo "}" >>amalgamation.cc
